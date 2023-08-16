d1aaa01877f223a7af1bce15d73aff517ca08494
admin <19973754931@163.com>
2023-08-16 10:57:00 +0800

    2023-08-26 10:55

---
+++ TravelNetwork/src/test/java/com/cssl/TravelNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cssl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
